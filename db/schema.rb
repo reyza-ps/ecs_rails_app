a940dd1009a8ac9a4d6da0d723019d4cae3b9a4e
reyzapermanasaputra <reyzaps@gmail.com>
2025-06-07 12:50:12 +0700

    add role

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_06_07_053645) do
+ActiveRecord::Schema[7.1].define(version: 2025_06_07_054733) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "roles", force: :cascade do |t|
+    t.string "name"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_roles_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -21,4 +29,5 @@ ActiveRecord::Schema[7.1].define(version: 2025_06_07_053645) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "roles", "users"
 end
